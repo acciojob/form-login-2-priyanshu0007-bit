2798b8301c36b489ddda4c0fcade09dd5860ad01
priyanshu ahir <priyanshuahir777@gmail.com>
2026-08-11 15:39:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 let submit = document.getElementById("button");
 
 function sub(event) {
-	event.preventDefault();
-	let firstName = document.querySelector(`input[name=firstName]`).value;
-	let lastName = document.querySelector(`input[name=lastName]`).value;
-    let phoneNumber = document.querySelector(`input[name=phoneNumber]`).value;
-	let email = document.querySelector(`input[name=email]`).value;
-	
-	alert(
-		"First Name: "+firstName+
-		"\nLast Name: "+lastName+
-		"\nphoneNumber: "+phoneNumber+
-		"\nemail: "+email
-	);
+    event.preventDefault();
+
+    let firstName = document.querySelector('input[name="First Name"]').value;
+    let lastName = document.querySelector('input[name="Last Name"]').value;
+    let phoneNumber = document.querySelector('input[name="Phone Number"]').value;
+    let email = document.querySelector('input[name="Email ID"]').value;
+
+    alert(
+        "First Name: " + firstName +
+        "\nLast Name: " + lastName +
+        "\nPhone Number: " + phoneNumber +
+        "\nEmail ID: " + email
+    );
 }
 
 submit.addEventListener("click", sub);
