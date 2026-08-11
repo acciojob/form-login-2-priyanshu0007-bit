d7505c1f2bf95fa27687d81d2e70e610596119ef
priyanshu ahir <priyanshuahir777@gmail.com>
2026-08-11 15:24:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+let submit = document.getElementById("button");
+
+function sub(event) {
+	event.preventDefault();
+	alert("Form submitted successfully");
+}
+
+submit.addEventListener("click", sub);
