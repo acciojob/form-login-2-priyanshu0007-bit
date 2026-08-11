c6ea66b4b91073796298ab13bab12998d3597090
priyanshu ahir <priyanshuahir777@gmail.com>
2026-08-11 15:33:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,17 @@ let submit = document.getElementById("button");
 
 function sub(event) {
 	event.preventDefault();
-	alert("Form submitted successfully");
+	let firstName = document.querySelector(`input[name=firstName]`).value;
+	let lastName = document.querySelector(`input[name=lastName]`).value;
+    let phoneNumber = document.querySelector(`input[name=phoneNumber]`).value;
+	let email = document.querySelector(`input[name=email]`).value;
+	
+	alert(
+		"First Name: "+firstName+
+		"\nLast Name: "+lastName+
+		"\nphoneNumber: "+phoneNumber+
+		"\nemail: "+email
+	);
 }
 
 submit.addEventListener("click", sub);
